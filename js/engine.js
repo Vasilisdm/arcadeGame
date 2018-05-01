f471e4742ea60c169b1a165efcffbcacbd796595
= <=>
2018-05-01 10:28:27 +0300

    appended the canvas of the game not to the body but to gameCanvas div

--- js/engine.js
+++ js/engine.js
@@ -23,10 +23,10 @@ var Engine = (function(global) {
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         lastTime;
-
+    
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    doc.querySelector('.gameCanvas').appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
