e0f64d5b15e6a35f1f3593a28a8e1bc982725c7b
= <=>
2018-05-02 22:48:05 +0300

    refactored the code for better understanding

--- js/app.js
+++ js/app.js
@@ -4,12 +4,7 @@ let Enemy = function(x, y, velX) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-    this.x = x;
-    this.y = y;
-
-    // enemy speed on x-axis
-    this.velX = velX;
+    this.reset();
 };
 
 // Update the enemy's position, required method for game
@@ -174,7 +169,7 @@ Player.prototype.addLives = function() {
 }
 
 // instatiating the allEnemies array
-let allEnemies = [];
+const allEnemies = [];
 
 // the value that is going to be subtracted from the y-axis position 
 // during instatiation of the enemy in order for the bug to be centred 
@@ -182,10 +177,7 @@ const bugOffset = 20;
 
 // creating enemies/bugs until their total number is 4 or smaller
 while (allEnemies.length <= 4) {
-
-    // instatiating the enemies with random y-position and velocity
-    let enemy = new Enemy(-101, random(1,3)*83-bugOffset, random(20,100));
-    allEnemies.push(enemy);
+    allEnemies.push(new Enemy());
 }
 
 // player instatiation
